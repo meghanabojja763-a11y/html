405454f0fa1d4702a5fe64fce859b894023ffe5c
meghanabojja763-a11y <meghanabojja763@gmail.com>
2026-01-28 14:58:53 +0530

    Delete backend/database.py

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "sqlite:///./data/attendance.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
